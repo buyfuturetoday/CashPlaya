4bec5c6d9e18b0ef7f4a8be72005bab9c73eeb3a
moz <moz@monlennart>
2013-05-02 18:00:57 +0200

    added pocket class added pockets generator class

--- CashPlaya/src/ObjectDetect.py
+++ CashPlaya/src/ObjectDetect.py
@@ -16,6 +16,7 @@ ScrewFilenameBotomRight = "Templates/BottomRightCornerScrew.png"
 color_red = (0, 0, 255)
 
 class point(object):
+    ''' point class: basic x,y with add and subtract '''
     def __init__(self, x,y):
         self.x = x
         self.y = y
@@ -30,8 +31,27 @@ class point(object):
     def tupple(self):
         return self.x, self.y
     
+class pocket( object ):
+    ''' the square thing to hold an item '''
+    def __init__(self, col, row, offset, size = 40 ):
+        self.PointTL = offset+point(col*size, row*size )
+        self.PointBR = offset+point((col+1)*size-1, (row+1)*size-1 )
+        self.pos = point( col, row )
+        self.offset = offset
+        self.size = size
+        
+    @property
+    def pointTopLeft(self):
+        return self.PointTL
+    
+    @property
+    def pointBottomRight(self):
+        return self.PointBR
 
 def FindCorner( image, screwtemplate, ShowTracking = True ):
+    ''' using the template, the corner screw is found
+    @return: the position of the corner screw
+    '''
     w,h = screwtemplate.shape[:2]
 
     result = cv2.matchTemplate(screwtemplate,image,cv2.TM_CCORR_NORMED)
@@ -45,6 +65,20 @@ def FindCorner( image, screwtemplate, ShowTracking = True ):
         cv2.rectangle(image, maxloc, (maxloc[0]+w, maxloc[1]+h), color_red )
     
     return point(maxloc[0], maxloc[1])
+
+def GenPockets( offset, ColCount = 7, RowCount = 7):
+    ''' generates the pocket objects with default data '''
+    pockets = []
+    for col in range(0,ColCount):
+        pockets.append([])
+        for row in range(0,RowCount):
+            pockets[col].append(pocket( col, row, offset ))
+
+    return pockets
+    
+def CheckPocket( col, row):
+    ''' checks if the '''
+    pass
 
 # load templates
 screwtemplateTL = cv2.imread(ScrewFilenameTopLeft)
@@ -74,19 +108,21 @@ for screenshotfilename in glob.glob("Screenshots/*.png"):
 
     # red squares in matrix
     offset = point( DropItemsAreaTL.x, DropItemsAreaBR.y )
-    # TODO: magic values!
-    size = 40   # square areas
+    
+    pockets = GenPockets( offset )
+
+    # draw pockets
     for col in range(0,7):
         for row in range(0,7):
             cv2.rectangle(image, 
-                          (offset+point(col*size, row*size )).tupple, 
-                          (offset+point((col+1)*size, (row+1)*size )).tupple, 
+                          pockets[col][row].pointTopLeft.tupple,
+                          pockets[col][row].pointBottomRight.tupple,
                           color_red )
-
+            # TODO: add text here to show what the detected content is.
         
     # TODO: add rectangle aroung next items
     
-    cv2.imshow("image", image)
+    cv2.imshow(screenshotfilename, image)
 
     
 # wait and cleanup
