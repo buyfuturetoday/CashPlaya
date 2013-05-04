a27d0a5e190bb700da81c50a308caea39b8160f9
moz <moz@monlennart>
2013-05-04 22:46:37 +0200

    added filepattern as argument for itemlist

--- CashPlaya/src/data/ItemList.py
+++ CashPlaya/src/data/ItemList.py
@@ -7,42 +7,30 @@ import numpy
 
 class ItemList(object):
         
-    def __init__(self, itempath):
+    def __init__(self, itempath, filepattern = "item_%02d.bmp"):
         self.path = itempath
-        
+        self.filepattern = filepattern
         self.initImages( )
-        pass
     
     def initImages(self):
         itemcounter = 0
         images = []
-        height = 0
         width = 0
         
         # loop until no more files
         while True:
-            filename = "%s/item_%02d.bmp"%(self.path, itemcounter)
+            filename = os.path.join( self.path, self.filepattern%itemcounter)
 
             if not os.path.isfile( filename ):
                 break
 
             images.append( cv2.imread( filename ) )
-
-#             height = height + images[-1].shape[0]            
-#             if images[-1].shape[0] > width:
-#                 width = images[-1].shape[0]
             itemcounter = itemcounter + 1
             
         # and save the result        
         self.imagecount = itemcounter
         self.itemsize = width
-#         self.allImages = numpy.zeros((height, width, 3), numpy.uint8)
         self.allImages = images
-                
-#         h, w = images[0].shape[:2]
-#         for i in range(0, self.imagecount):
-#             self.allImages[i*h:(i+1)*h, :w ] = images[i]
-        
 
     @property
     def count(self):
@@ -50,24 +38,10 @@ class ItemList(object):
 
     
     def findItem(self, imToFind):
-#         result = cv2.matchTemplate(self.allImages, imToFind,cv2.TM_CCORR_NORMED)
-#         minval, maxval, minloc, maxloc = cv2.minMaxLoc(result)  # @UnusedVariable
-# 
-# #         # TODO: magic value...
-# #         if maxval < 0.8:
-# #             return None
-#         print maxloc
-#         print maxval
-# 
-#         # make a drawing, it will explain it.
-#         return round(maxloc[1]/float(self.itemsize) + 0.49)
-
         maxval = 0
         maximg = 0
         for i in range( 0, len(self.allImages)):
-            #print i
             result = cv2.matchTemplate(self.allImages[i], imToFind,cv2.TM_CCORR_NORMED)
-            #print result
             if maxval < result[0]:
                 maxval = result[0]
                 maximg = i
