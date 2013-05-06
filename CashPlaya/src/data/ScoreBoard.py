1074d65741baee92fe184a184384708b0d77fa15
moz <moz@monlennart>
2013-05-06 21:34:42 +0200

    now detected score correctly

--- CashPlaya/src/data/ScoreBoard.py
+++ CashPlaya/src/data/ScoreBoard.py
@@ -1,34 +1,96 @@
 from data.BoardElement import BoardElement
 from data.point import point
 from data.Digit import Digit
+import cv2
+import sys
 
 class ScoreBoard(BoardElement):
     _size = point(184, 39)
-    _digit_rel_loc = [  point( 160, 4 ), 
-                        point( 143, 4 ), 
-                        point( 126, 4 ), 
-                        point(  96, 4 ), 
-                        point(  79, 4), 
-                        point(  62, 4 ), 
-                        point(  32, 4 ) ]
+    _digit_rel_loc = [  point( 160, 5 ), 
+                        point( 143, 5 ), 
+                        point( 126, 5 ), 
+                        point(  95, 5 ), 
+                        point(  78, 5), 
+                        point(  61, 5 ), 
+                        point(  30, 5 ) ]
+    _defaultDigitSize = point(20, 29 )
     
     def __init__(self, offset, digitlist ):
         ''' constructor
         ''' 
         super(ScoreBoard, self).__init__( offset, self._size)
-        defaultDigitSize = point(15, 30 )
+        
         
         self._digitlist = digitlist
         
+        # bogus boardelements - specific location is fixed later.
         self._digitelements = []
         for digit_loc in self._digit_rel_loc:
-            self._digitelements.append( Digit(self.pointTopLeft+digit_loc, defaultDigitSize ))
-        
-#     def readScore(self, image):
-#         for location in digit_location:
-#             digit_img = 
+            self._digitelements.append( Digit(self.pointTopLeft+digit_loc, self._defaultDigitSize ))
 
     def ShowBoundary(self, color, overlayImage):
         super(ScoreBoard, self).ShowBoundary(color, overlayImage)
         for digit in self._digitelements:
             digit.ShowBoundary( color, overlayImage)
+    
+    def ReadScore(self, image ):
+        digits = []
+        widthadj = 0
+        for i in range( 0, len( self._digit_rel_loc ) ):
+            imToFind = self._digitelements[i].getImage( image )
+
+            item = self._digitlist.findItem( imToFind )
+            if item == None:
+                filename = 'digit_loc_%02d.png'%i
+                cv2.imwrite( filename, imToFind )
+                print >> sys.stderr, "digit not recognized: %s"%filename
+            else:
+                digits.append( item )
+
+                res = self._digitlist.getLastResult()[item]
+                print "item: %d, result: %f, pos %s"%(item, res[1], res[3])
+
+                size = point( *self._digitlist.getSize( item ) )
+#                widthadj = widthadj + self._defaultDigitSize.x - size.x
+                widthadj = widthadj + res[3][0]
+
+                # update current element with new size
+                # size is known by detected item.
+                # TopLeft is oldBottomRight-size+p(1,1) - where it was detected
+#                 self._digitelements[i] = Digit(self._digitelements[i].pointBottomRight-size+point(1,1)-point(res[3][0], res[3][1]), 
+#                                                size )
+                self._digitelements[i] = Digit(self._digitelements[i].pointTopLeft+point(res[3][0], res[3][1]), 
+                                               size )
+
+                print "digit %d width: %d (acc: %d)"%(item, size.x, widthadj)
+#                 print self._digitelements[i].pointTopLeft
+#                 print self._digitelements[i].pointBottomRight
+#                                 
+#                 print self._digitelements[i].pointBottomRight   \
+#                                                  - point( self._defaultDigitSize.x, 0 )
+
+                
+            if i+1 < len( self._digit_rel_loc ):
+                
+                if (i+1) % 3 == 0:
+                    ExtraOffset = point( 12+3, 0 )
+                else:
+                    ExtraOffset = point( 0+3, 0 )                    
+                
+                # recreate digit with better start point
+                self._digitelements[i+1] = Digit(self._digitelements[i].pointTopLeft    \
+                                                 - point( self._defaultDigitSize.x, 0) - ExtraOffset,
+                                                 self._defaultDigitSize )
+
+#             cv2.imshow("imtofind", imToFind)
+#             cv2.waitKey()
+        
+        for digit in self._digitelements:
+            print digit.pointTopLeft
+        print digits
+        digitssum = 0
+        for i in range( 0, len(digits) ):
+            digitssum = digitssum + digits[i]*10**i
+        return digitssum
+    
+    
--- CashPlaya/src/tests/data/testScoreBoard.py
+++ CashPlaya/src/tests/data/testScoreBoard.py
@@ -11,11 +11,23 @@ import cv2
 import numpy
 
 offsetPoint = point( 10, 20 )
-il = ItemList( 'data/digits', "digit_%d.bmp")
-digit = cv2.imread( 'data/digits/digit_1.bmp' )
+il = ItemList( 'testdata/digits', "digit_%d.png")
+il.setThreshold(0.8)
+digit = cv2.imread( 'testdata/digits/digit_0.png' )
+assert( digit is not None )
 
-scoreboard_A = cv2.imread( 'data/digits/scoreboard_369350.bmp' )
+scoreboard_A = cv2.imread( 'testdata/digits/ScoreBoard_369350.png' )
 scoreboard_A_score = 369350
+assert( scoreboard_A is not None )
+scoreboard_B = cv2.imread( 'testdata/digits/ScoreBoard_105.png' )
+scoreboard_B_score = 105
+assert( scoreboard_B is not None )
+scoreboard_C = cv2.imread( 'testdata/digits/ScoreBoard_248010.png' )
+scoreboard_C_score = 248010
+assert( scoreboard_C is not None )
+scoreboard_D = cv2.imread( 'testdata/digits/ScoreBoard_177000.png' )
+scoreboard_D_score = 177000
+assert( scoreboard_D is not None )
 
 class Test(unittest.TestCase):
 
@@ -30,13 +42,43 @@ class Test(unittest.TestCase):
         
         sb = ScoreBoard( offsetPoint, il )
         sb.ShowBoundary(color, bigImage)
-        
-#     def testReadScore(self):
-#         
-#         sb = ScoreBoard( point( 3,2 ), il )
-#         sb.ReadScore( scoreboard_A )
-        
-        
+         
+    def testReadScoreA(self):
+        sb = ScoreBoard( point( 0,0 ), il )
+        scoreread = sb.ReadScore( scoreboard_A )
+ 
+#         sb.ShowBoundary((0,0,255), scoreboard_A)
+#         cv2.imshow("A", scoreboard_A)
+#         cv2.waitKey()
+         
+        self.assertEqual(scoreboard_A_score, scoreread)
+          
+    def testReadScoreB(self):
+        sb = ScoreBoard( point( 0,0 ), il )
+        scoreread = sb.ReadScore( scoreboard_B )
+  
+#         sb.ShowBoundary((0,0,255), scoreboard_B)
+#         cv2.imshow("B", scoreboard_B)
+#         cv2.waitKey()
+          
+        self.assertEqual(scoreboard_B_score, scoreread)   
+         
+    def testReadScoreC(self):
+        sb = ScoreBoard( point( 0,0 ), il )
+        scoreread = sb.ReadScore( scoreboard_C )
+        self.assertEqual(scoreboard_C_score, scoreread)   
+           
+    def testReadScoreD(self):
+        sb = ScoreBoard( point( 0,0 ), il )
+        scoreread = sb.ReadScore( scoreboard_D )
+  
+#         sb.ShowBoundary((0,0,255), scoreboard_D)
+#         cv2.imshow("B", scoreboard_D)
+#         cv2.imwrite("scoreB.png", scoreboard_D)
+#         cv2.waitKey()
+         
+        self.assertEqual(scoreboard_D_score, scoreread)   
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testScoreBoard']
     unittest.main()
